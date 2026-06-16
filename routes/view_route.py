d901f5c57a486b82f0ba602f4b74bfaaddef34a6
JeongMin-Lee3 <eorjarnl123@cu.ac.kr>
2026-06-16 16:49:40 +0900

    0616 html/css 불태웠다

--- routes/view_route.py
+++ routes/view_route.py
@@ -7,3 +7,19 @@ view_bp = Blueprint('view', __name__, url_prefix='/')
 @view_bp.route('/')
 def index():
     return render_template('index.html')
+
+@view_bp.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@view_bp.route('/basic-1')
+def basic_1():
+    return render_template('basic-1.html')
+
+@view_bp.route('/class-id')
+def class_id():
+    return render_template('class-id.html')
+
+@view_bp.route('/layout')
+def layout():
+    return render_template('layout.html')
